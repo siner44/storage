beb981ae0fac525ad8640ac9a350ba3a20b72f8b
siner44 <92347765+siner44@users.noreply.github.com>
2021-10-14 13:31:48 +0900

    Update Member.java

--- java_project/FirstJava/src/chapter05/Member.java
+++ java_project/FirstJava/src/chapter05/Member.java
@@ -1,41 +1,48 @@
 package chapter05;
 
 public class Member {
-	 String name;
-	 String phoneNumber;
-	 String major;
-	 int grade;
-	 String mail;
-	 String birth;
-	 String address;
+	String name;
+	String phoneNumber;
+	String major;
+	int grade;
+	String mail;
+	String birth;
+	String address;
 
 	void printMember() {
 		System.out.println("이름: " + name);
 		System.out.println("전화번호: " + phoneNumber);
 		System.out.println("전공: " + major);
-		System.out.println("학년: " + grade);
+		System.out.println("학년: " + grade + "학년");
 		System.out.println("이메일: " + mail);
 		System.out.println("생일: " + birth);
 		System.out.println("주소: " + address);
 	}
 
-	Member() {
-		
+	Member(String name, String phoneNumber, String major, int grade, String mail) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.major = major;
+		this.grade = grade;
+		this.mail = mail;
+
 	}
-	
-	Member(String birth, String address) {
+
+	Member(String name, String phoneNumber, String major, int grade, String mail, String birth, String address) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.major = major;
+		this.grade = grade;
+		this.mail = mail;
 		this.birth = birth;
 		this.address = address;
 	}
 
 	public static void main(String[] args) {
-		
-		String birth = "2000.01.01";
-		String address = "서울시 강남구";
-		
-		Member member = new Member();
-		Member member2 = new Member(birth, address);
-		
+
+		Member member = new Member("신승민", "010-0000-0000", "국어", 4, "asd123@gmail.com");
+		Member member2 = new Member("홍길동", "010-0000-0000", "수학", 2, "fgh456@gmail.com", "01.01", "서울시 강남구");
+
 		member.printMember();
 		System.out.println("---------------------------------");
 		member2.printMember();

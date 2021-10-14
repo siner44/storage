749f6b929d304505ec81b6f528f8006f6179f9d8
siner44 <92347765+siner44@users.noreply.github.com>
2021-10-14 12:30:44 +0900

    Update Exam01.java

--- java_project/FirstJava/src/exam/Exam01.java
+++ java_project/FirstJava/src/exam/Exam01.java
@@ -15,7 +15,7 @@ public class Exam01 {
 		System.out.println();
 		System.out.println("----------------------------------");
 
-		// 다음 형태로 주민번호를 담고 있는 String 인스턴스를 하나 생성
+		// 문제 2. 다음 형태로 주민번호를 담고 있는 String 인스턴스를 하나 생성
 		// String str = " 990929-1010123";
 		// 이 문자열을 이용하여 중간에 삽입된 -를 뺀 String 인스턴스를 생성
 

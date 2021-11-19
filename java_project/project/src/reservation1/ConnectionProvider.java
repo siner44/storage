5a98dc0bf97c3c363205a293af71903cd8661478
siner44 <92347765+siner44@users.noreply.github.com>
2021-11-19 15:50:28 +0900

    edit

--- java_project/project/src/reservation1/ConnectionProvider.java
+++ java_project/project/src/reservation1/ConnectionProvider.java
@@ -8,8 +8,8 @@ public class ConnectionProvider {
 	public static Connection getConnection() throws SQLException {
 		// String jdbcUr1 = "jdbc:oracle:thin:@[주소]:[포트]:[데이터베이스]";
 		String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe";
-		String user = "SIN";
-		String pw = "admin";
+		String user = "scott";
+		String pw = "tiger";
 
 		return DriverManager.getConnection(jdbcUrl, user, pw);
 	}

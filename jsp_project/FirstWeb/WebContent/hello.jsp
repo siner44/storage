f253c5f68a921151e490b6155743b192bfbb9a12
siner44 <92347765+siner44@users.noreply.github.com>
2021-12-06 18:27:29 +0900

    20211206 servlet

--- jsp_project/FirstWeb/WebContent/hello.jsp
+++ jsp_project/FirstWeb/WebContent/hello.jsp
@@ -10,13 +10,14 @@
 </head>
 <body>
 	<% 
-		// java 코드를 작성 
-		// java.util.Date now1 = new java.util.Date();
+		// java 코드를 작성
+		//java.util.Date now1 = new java.util.Date();
 		Date now1 = new Date();
-	
+			
 	%>
 	<%= now1 %>
 	<h1><%= LocalDateTime.now() %></h1>
+	
 	
 	<h1>처음 작성한 JSP 파일</h1>
 </body>
---
+++ jsp_project/FirstWeb/src/first/HelloServlet.java
@@ -0,0 +1,40 @@
+package first;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(
+			HttpServletRequest req, 
+			HttpServletResponse resp) 
+					throws ServletException, IOException {
+		System.out.println("Get 요청 처리!!!");
+		
+		resp.getWriter().println("<html>");
+		resp.getWriter().println("<head>");
+		resp.getWriter().println("<title>Servlet</title>");
+		
+		Date now = new Date();
+		
+		resp.getWriter().println("</head>");
+		resp.getWriter().println("<body>");
+		resp.getWriter().println("<h1>Get Request !!!"+now+"</h1>");
+		resp.getWriter().println("</body>");
+		resp.getWriter().println("</html>");
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+	
+}

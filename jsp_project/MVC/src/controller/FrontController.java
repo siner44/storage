1396ce6d8d3e844d8e1f713adcfb4e8c784210d7
siner44 <92347765+siner44@users.noreply.github.com>
2021-12-14 14:45:13 +0900

    20211214 mvc

--- jsp_project/MVC/src/controller/FrontController.java
+++ jsp_project/MVC/src/controller/FrontController.java
@@ -1,67 +1,120 @@
 package controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
 
-import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.BadRequestCommandImpl;
+import service.Command;
+import service.DateCommandImpl;
+import service.GreetingCommandImpl;
+
 public class FrontController extends HttpServlet {
 
+	private Map<String, Command> commands = new HashMap<String, Command>();
+
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("서블릿이 생성될때 한번 실행합니다 -> 초기화 처리합니다");
+
+		// 설정 파일에 init-param 설정된 경로 가져오기
+		String configFile = config.getInitParameter("configPath");
+
+		// 시스템 경로 실제 경로
+		String configPath = config.getServletContext().getRealPath(configFile);
+		// 프로퍼티(MAP 파생된 클래스) 파일 생성
+		// 파일 -> 객체
+		Properties prop = new Properties();
+
+		FileInputStream fis = null;
+
+		try {
+			fis = new FileInputStream(configPath);
+			// 파일 -> 객체 Properties
+			prop.load(fis);
+			
+			fis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Iterator<Object> itr = prop.keySet().iterator();
+
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+		// Command 설정
+//		commands.put("/date.do", new DateCommandImpl());
+//		commands.put("greeting.do", new GreetingCommandImpl());
+
+		System.out.println("초기화된 요청 : " + commands.size());
+
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// 1.사용자 요청 받기
 		doProcess(request, response);
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// 1. 사용자 요청 받기
 		doProcess(request, response);
 	}
 
-	private void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// 2. 사용자 요청 분석: uri를 이용해서 사용자 요청 분석
 		String commandURI = request.getRequestURI();
 		// http://localhost:8080//mvc/simple.do : commandURI => /mvc/simple.do
 		// http://localhost:8080//mvc/greeting.do : commandURI => /mvc/greeting.do
-		
-		if(commandURI.startsWith(request.getContextPath())) {
+
+		if (commandURI.startsWith(request.getContextPath())) {
 			commandURI = commandURI.substring(request.getContextPath().length());
 		}
-		
+
 		// 3. 사용자 요청에 맞는 데이터 처리: 요청에 따른 분기
 		// 결과 데이터를 생성
-		
+
 		// 응답 결과 객체
-		Object result = null; // view 페이지에서 toString()으로 출력
+//		Object result = null; // view 페이지에서 toString()으로 출력
 		// view page 경로
-		String viewPage = "/WEB-INF/views/simpleview.jsp";
-		
-		
-		if(commandURI.equals("/greeting.do")) {
-			result = "안녕하세요."; // object < - String 
-			viewPage = "/WEB-INF/views/greeting.jsp";
-		} else if (commandURI.equals("date.do")) {
-			result = new Date(); // object < - Date
-			viewPage = "/WEB-INF/views/date.jsp";
-		} else { // /*.do
-			result = "Invalid request";
+
+		Command command = commands.get(commandURI);
+		if (command == null) {
+			command = new BadRequestCommandImpl();
 		}
-		
-		// 4. 결과를 request 객체의 속성에 저장: view 페이지에 결과 데이터를 공유(전달)
-		request.setAttribute("result", result);
-		
+
+//		if(commandURI.equals("/greeting.do")) {
+//			command = new GreetingCommandImpl();
+//			
+//		} else if (commandURI.equals("date.do")) {
+//			command = new DateCommandImpl();
+//		} else { // /*.do
+//			command = new BadRequestCommandImpl();
+//		}
+
+		String viewPage = command.getPage(request, response);
+
 		// 5. view 페이지를 지정(선택) -> 포워딩
 		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
 		dispatcher.forward(request, response);
-		
-		
+
 	}
-	
-	
+
 }
---
+++ jsp_project/MVC/src/service/Command.java
@@ -0,0 +1,13 @@
+package service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Command {
+
+	// 추상 메소드 : public abstract
+	// 반환하는 데이터는 viewPage의 경로
+	// 내부 처리는 : 사용자 요청하는 데이터 처리 -> 결과 data를 request의 속성에 데이터 저장
+	String getPage(HttpServletRequest request, HttpServletResponse response);
+
+}

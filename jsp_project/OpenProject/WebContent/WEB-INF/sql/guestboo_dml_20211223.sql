aa484990654bcafd966ec9585e3624f19135dfb3
siner44 <92347765+siner44@users.noreply.github.com>
2021-12-27 18:54:58 +0900

    20211227 ajax

---
+++ jsp_project/OpenProject/WebContent/WEB-INF/sql/guestboo_dml_20211223.sql
@@ -0,0 +1,54 @@
+SELECT * FROM project.guestbook;
+
+# insert
+INSERT INTO guestbook (subject, content, memberidx) VALUES (?,?,?)
+;
+
+
+# select List
+select 
+	g.idx as idx , 
+    g.subject as subject, 
+    g.content as content, 
+    m.username as username, 
+    g.regdate as regdate,
+    m.photo as photo 
+from project.guestbook g join project.member m
+on g.memberidx=m.idx
+order by regdate desc
+limit 0, 2
+;
+select g.idx as idx , g.subject as subject, g.content as content, g.regdate as regdate, m.username as username, m.photo as photo from project.guestbook g join project.member m on g.memberidx=m.idx order by regdate desc
+limit 0, 2
+;
+
+
+# select idx
+select 
+	g.idx as idx , 
+    g.subject as subject, 
+    g.content as content, 
+    m.username as username, 
+    g.regdate as regdate,
+    m.photo as photo ,
+    m.idx as memberidx,
+    m.userid as userid
+from project.guestbook g join project.member m
+on g.memberidx=m.idx
+where g.idx=3
+;
+select g.idx as idx, g.subject as subject, g.content as content, m.username as username, g.regdate as regdate, m.photo as photo, m.idx as memberidx, m.userid as userid from project.guestbook g join project.member m on g.memberidx=m.idx where g.idx=3
+;
+
+select g.idx as idx, g.subject as subject, g.content as content, m.username as username, g.regdate as regdate, m.photo as photo, m.idx as memberidx, m.userid as userid from project.guestbook g join project.member m on g.memberidx=m.idx where g.idx=?
+;
+
+
+
+-- 아이디 중복 체크 sql
+select count(*) from project.member where userid='test@gmail.com';
+select count(*) from project.member where userid=?;
+
+select * from project.member;
+
+select * from project.guestbook;

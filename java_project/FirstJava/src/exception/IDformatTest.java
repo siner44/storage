067fc05176c08a6e493327c38d8f5ef1ae63faf3
siner44 <92347765+siner44@users.noreply.github.com>
2022-05-18 23:42:50 +0900

    220518 exception

---
+++ java_project/FirstJava/src/exception/IDformatTest.java
@@ -0,0 +1,43 @@
+package exception;
+
+public class IDformatTest {
+
+	private String userID;
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) throws IDformatException {
+
+		if (userID == null) {
+			throw new IDformatException("아이디는 null 일 수 없습니다.");
+		} else if (userID.length() < 8 || userID.length() > 20) {
+			throw new IDformatException("아이디는 8자 이상 20자 이하로 쓰세요.");
+		}
+
+		this.userID = userID;
+	}
+
+	public static void main(String[] args) {
+
+		IDformatTest idTest = new IDformatTest();
+		String userID = null;
+
+		try {
+			idTest.setUserID(userID);
+		} catch (IDformatException e) {
+			System.out.println(e);
+		}
+
+		userID = "1234567";
+
+		try {
+			idTest.setUserID(userID);
+		} catch (IDformatException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}

3074c5e6b0508b6147ccddc528493be07d0c8efb
siner44 <92347765+siner44@users.noreply.github.com>
2022-07-20 23:32:18 +0900

    20220720 collection

---
+++ java_project/FirstJava/src/collection/HashSetTest.java
@@ -0,0 +1,24 @@
+package collection;
+
+import java.util.HashSet;
+
+public class HashSetTest {
+	public static void main(String[] args) {
+		
+		HashSet<String> set = new HashSet<String>();
+		
+		boolean b1 = set.add("aaa");
+		
+		System.out.println(b1);
+		
+		set.add("bbb");
+		set.add("ccc");
+	
+		System.out.println(set);
+		
+		boolean b = set.add("aaa");
+		
+		System.out.println(b);
+		
+	}
+}
